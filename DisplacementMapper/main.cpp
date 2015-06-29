069d72c220288df0676a72ec5a8c7be96fa3e87b
Nate Sutton <natemsutton@gmail.cm>
2015-06-28 22:23:40 -0400

    refining further disp map translations

--- DisplacementMapper/main.cpp
+++ DisplacementMapper/main.cpp
@@ -48,7 +48,7 @@ void handleKeypress(unsigned char key, int x, int y) {
 	}
 }
 
-String imageName6 = "/home/nmsutton/Documents/Software/OpenGL/Media/GeneralProcessed2.bmp";
+String imageName6 = "/home/nmsutton/Documents/Software/OpenGL/Media/GeneralProcessed.bmp";
 Mat dispMapImage;
 
 //Makes the image into a texture, and returns the id of the texture
@@ -128,7 +128,7 @@ void drawScene() {
 
 	// camera position mapper
 	//glTranslatef(-30.0f, 1.0f, -60.0f);//-150.0f);
-	glTranslatef(-30.0f, -4.0f, -45.0f);//-150.0f);
+	glTranslatef(-25.0f, 11.0f, -90.0f);//-150.0f);
 
 	//GLfloat ambientLight[] = {0.2f, 0.2f, 0.2f, 1.0f};
 	GLfloat ambientLight[] = {200.2f, 200.2f, 200.2f, 1.0f};
@@ -158,13 +158,13 @@ void drawScene() {
 
 	vertsAndTextures vAT3;
 
-	double sizeScalingFactor = 5;
-	double sizeOfMesh = 6;//50;
-	sizeOfMesh = sizeOfMesh/sizeScalingFactor;
+	//double sizeScalingFactor = 5;
+	double sizeOfMesh = 10;//6;//50;
+	//sizeOfMesh = sizeOfMesh/sizeScalingFactor;
 	double sizeOfMesh2 = sizeOfMesh*2;
 	//double x = 0, y = 0;
 	int x = 0, y = 0;
-	double expandMeshSize = 2.5;
+	double expandMeshSize = 2.0;//2.5;
 	float initalZ = 40.0f;
 	double depthScalingFactor = .025;//.3;//0.1;
 
@@ -219,39 +219,59 @@ void drawScene() {
 	//double incrementValue = 0.5;
 	int incrementValue = 1;
 
-	// Build displacement map.  *4 is due to 4 rectangle verticies for each Z level included.
-	int verticiesInRectangle = 4;
-	int xAmount = ceil(sizeOfMesh*expandMeshSize*(1/incrementValue))*4, yAmount = ceil(sizeOfMesh*(1/incrementValue))*4;
+	// Build displacement map.  *2 is due to 2 rectangle verticies for each x and y axis used for the later Z level mapping.
+	double verticiesInRectangle = 2;
+	int xAmount = ceil(sizeOfMesh*expandMeshSize*(1/incrementValue))*2, yAmount = ceil(sizeOfMesh*(1/incrementValue))*2;
 	double verZLevels[yAmount][xAmount] = {0};
 	for (int y = 0; y < yAmount; y += 2) {
 		for (x = 0; x < xAmount; x += 2) {
-			double imageToScenePixelDiff = (255/sizeOfMesh)*(1/verticiesInRectangle);
+			double imageToScenePixelDiff = (255/(double)sizeOfMesh)*(1/verticiesInRectangle);
 			//double translatedX = x * imageToScenePixelDiff; double translatedY = y * imageToScenePixelDiff;
 			double translatedX = ceil(x * imageToScenePixelDiff); double translatedY = ceil(y * imageToScenePixelDiff);
+			////cout << "translatedX\t";cout<<translatedX;cout<<"\ttranslatedY\t";cout<<translatedY;cout<<"\n";
 			//double nextTranslatedX = (x+incrementValue) * imageToScenePixelDiff; double nextTranslatedY = (y+incrementValue) * imageToScenePixelDiff;
 			//double priorTranslatedX = (x-incrementValue) * imageToScenePixelDiff; double priorTranslatedY = (y-incrementValue) * imageToScenePixelDiff;
-			int meshVec_0_0_b = dispMapImage.at<Vec3b>(translatedY,translatedX).val[0];
+			double meshVec_0_0_b = dispMapImage.at<Vec3b>((int)translatedY,(int)translatedX).val[0];
+			//meshVec_0_0_b = meshVec_0_0_b/3;
+			meshVec_0_0_b = meshVec_0_0_b/4;
 
+			//cout<<"\ny: ";cout<<y;cout<<" x: ";cout<<x;cout<<"\n";
+
+			////cout<<"[y+1][x+1]\t";cout<<x+1;cout<<" ";cout<<y+1;cout<<" ";cout<<" meshVec_0_0 ";cout<<meshVec_0_0_b;cout<<"\n";
 			if (x < xAmount) {
-				verZLevels[y][x] = meshVec_0_0;
+				verZLevels[y][x] = meshVec_0_0_b;
 			}
 			if (x > 0) {
-				verZLevels[y][x+1] = meshVec_0_0;
+				verZLevels[y][x+1] = meshVec_0_0_b;
 			}
 			if ((y < yAmount) & (x < xAmount)) {
-				verZLevels[y+1][x+1] = meshVec_0_0;
+				verZLevels[y+1][x+1] = meshVec_0_0_b;
 			}
 			if (y > 0) {
-				verZLevels[y+1][x] = meshVec_0_0;
+				verZLevels[y+1][x] = meshVec_0_0_b;
 			}
 		}
 	}
-	cout << "yAmount\t";cout << yAmount;
-	cout<<"\txAmount\t";cout<<sizeOfMesh;//ceil(sizeOfMesh*expandMeshSize*(1/incrementValue));
-	cout<<"\n";
+	//cout << "yAmount\t";cout << yAmount;
+	//cout<<"\txAmount\t";cout<<xAmount;
+	//cout<<"\n";
+	for (int y = 0; y < yAmount; y += 1) {
+		for (x = 0; x < xAmount; x += 1) {
+			double imageToScenePixelDiff = (255/(double)sizeOfMesh)*(1/verticiesInRectangle);
+			double translatedX = ceil(x * imageToScenePixelDiff); double translatedY = ceil(y * imageToScenePixelDiff);
+			double meshVec_0_0_b = dispMapImage.at<Vec3b>((int)translatedY,(int)translatedX).val[0];
+			//cout<<"(";cout<<y;cout<<",";cout<<x;cout<<"):";
+			cout<<"\t";cout<<meshVec_0_0_b;
+		}
+		cout<<"\n";
+	}
+	cout<<"_ _ _\n";
 
-
-	for (y = 0; y < sizeOfMesh; y += incrementValue) {
+	int scalingF = 2;
+	int incrementValue2 = 1;
+	double maxYSize = sizeOfMesh*scalingF;
+	double maxXSize = (sizeOfMesh*expandMeshSize*scalingF);
+	for (y = 0; y < (maxYSize); y += incrementValue2) {
 		//createMesh(vAT3);
 		// Reinitialze X dimension before the x value loop
 		vAT3.ULVerInst[0] = initVerXUL;
@@ -266,7 +286,7 @@ void drawScene() {
 		//if (x==0 & y==0) {createMesh(vAT3);}
 
 		// For y dimension iteration update the y values
-		if (y < sizeOfMesh/2) {
+		if (y < maxYSize/2) {
 			vAT3.ULVerInst[1] -= verYIncrement;
 			vAT3.URVerInst[1] -= verYIncrement;
 			vAT3.BLVerInst[1] -= verYIncrement;
@@ -282,7 +302,7 @@ void drawScene() {
 			vAT3.BLTexInst[1] -= texYIncrement;
 			vAT3.BRTexInst[1] -= texYIncrement;
 		}
-		else if (y >= sizeOfMesh/2) {
+		else if (y >= maxYSize/2) {
 			//cout<<"y greater";cout<<"\n";
 			vAT3.ULVerInst[1] -= verYIncrement;
 			vAT3.URVerInst[1] -= verYIncrement;
@@ -301,8 +321,8 @@ void drawScene() {
 		}
 
 
-		for (x = 0; x < sizeOfMesh*expandMeshSize; x += incrementValue) {
-			if ((x < sizeOfMesh/2) | (x < (sizeOfMesh/2+(sizeOfMesh*expandMeshSize-sizeOfMesh)) & x > (sizeOfMesh*expandMeshSize-sizeOfMesh))) {
+		for (x = 0; x < maxXSize; x += incrementValue2) {
+			if ((x < maxYSize/2) | (x < (maxYSize/2+(maxXSize-maxYSize)) & x > (maxXSize-maxYSize))) {
 				vAT3.ULVerInst[0] += verXIncrement;
 				vAT3.URVerInst[0] += verXIncrement;
 				vAT3.BLVerInst[0] += verXIncrement;
@@ -318,7 +338,7 @@ void drawScene() {
 				vAT3.BLTexInst[0] += texXIncrement;
 				vAT3.BRTexInst[0] += texXIncrement;
 			}
-			else if ((x >= sizeOfMesh/2 & x < sizeOfMesh) | (x > (sizeOfMesh/2+(sizeOfMesh*expandMeshSize-sizeOfMesh)) & x < (sizeOfMesh*expandMeshSize))) {
+			else if ((x >= maxYSize/2 & x < maxYSize) | (x > (maxYSize/2+(maxXSize-maxYSize)) & x < (maxXSize))) {
 				//cout<<"x greater";cout<<"\n";
 				vAT3.ULVerInst[0] += verXIncrement;
 				vAT3.URVerInst[0] += verXIncrement;
@@ -335,7 +355,7 @@ void drawScene() {
 				vAT3.BLTexInst[0] += texXIncrement;
 				vAT3.BRTexInst[0] += texXIncrement;
 			}
-			else if (x > (sizeOfMesh) & x < (sizeOfMesh*expandMeshSize-sizeOfMesh)) {
+			else if (x > (maxYSize) & x < (maxXSize-maxYSize)) {
 				vAT3.ULVerInst[0] += verXIncrement;
 				vAT3.URVerInst[0] += verXIncrement;
 				vAT3.BLVerInst[0] += verXIncrement;
@@ -356,23 +376,23 @@ void drawScene() {
 
 			// Apply displacement map
 
-			double imageToScenePixelDiff = 255/sizeOfMesh;
+			double imageToScenePixelDiff = 255/maxYSize;
 			double translatedX = x * imageToScenePixelDiff; double translatedY = y * imageToScenePixelDiff;
 			double nextTranslatedX = (x+incrementValue) * imageToScenePixelDiff; double nextTranslatedY = (y+incrementValue) * imageToScenePixelDiff;
 			double priorTranslatedX = (x-incrementValue) * imageToScenePixelDiff; double priorTranslatedY = (y-incrementValue) * imageToScenePixelDiff;
 			/*grayLevel = dispMapImage.at<Vec3b>(translatedY,translatedX).val[0];
-			if (x != (sizeOfMesh-1)*expandMeshSize) {
+			if (x != (maxYSize-1)*expandMeshSize) {
 				nextGrayLevel = dispMapImage.at<Vec3b>(nextTranslatedY,nextTranslatedX).val[0];
 			}*/
 			// Avoid edge problems
 			double tempVal = 0;
-			/*if (x >= ((sizeOfMesh-1)*expandMeshSize)) {
+			/*if (x >= ((maxYSize-1)*expandMeshSize)) {
 				nextTranslatedX = tempVal;//translatedX;
 			}
 			if (x == 0) {
 				priorTranslatedX = tempVal;//translatedX;
 			}
-			if (y >= ((sizeOfMesh-1))) {
+			if (y >= ((maxYSize-1))) {
 				nextTranslatedY = tempVal;//translatedY;
 			}
 			if (y == 0) {
@@ -473,11 +493,28 @@ void drawScene() {
 			vAT3.BLVerInst[2] = (meshVec_neg1_0) * depthScalingFactor+initalZ;//ur
 			vAT3.BRVerInst[2] = (meshVec_neg1_0) * depthScalingFactor+initalZ;//ul*/
 
-			vAT3.ULVerInst[2] = (meshVec_1_0) * depthScalingFactor+initalZ;//bl
+			/*vAT3.ULVerInst[2] = (meshVec_1_0) * depthScalingFactor+initalZ;//bl
 			vAT3.URVerInst[2] = ((meshVec_0_1+meshVec_1_0)/2) * depthScalingFactor+initalZ;//br  ((meshVec_0_1+meshVec_1_0)/2)
 			vAT3.BLVerInst[2] = (meshVec_0_1) * depthScalingFactor+initalZ;//ur
-			vAT3.BRVerInst[2] = ((meshVec_0_neg1+meshVec_neg1_0)/2) * depthScalingFactor+initalZ;//ul
+			vAT3.BRVerInst[2] = ((meshVec_0_neg1+meshVec_neg1_0)/2) * depthScalingFactor+initalZ;//ul*/
 
+			int x2 = x, y2 = y;
+			if (x2 < (maxXSize)) {
+				vAT3.BRVerInst[2] = verZLevels[y2][x2];//verZLevels[y2][x2] = meshVec_0_0;
+			}
+			if (x2 > 0) {
+				vAT3.BLVerInst[2] = verZLevels[y2][x2+1];//verZLevels[y2][x2+1] = meshVec_0_0;
+			}
+			if ((y2 < maxYSize) & (x2 < (maxXSize))) {
+				vAT3.URVerInst[2] = verZLevels[y2+1][x2+1];// = meshVec_0_0;
+			}
+			if (y2 > 0) {
+				vAT3.ULVerInst[2] = verZLevels[y2+1][x2];// = meshVec_0_0;
+			}
+			vAT3.BRVerInst[2] = verZLevels[y2][x2];
+			vAT3.BLVerInst[2] = verZLevels[y2][x2+1];
+			vAT3.URVerInst[2] = verZLevels[y2+1][x2+1];
+			vAT3.ULVerInst[2] = verZLevels[y2+1][x2];
 			/*cout<<"\n_______\n";
 			cout<<"x\t";cout<<x;cout<<"\ty\t";cout<<y;cout<<"\n";
 			cout<<"\tmeshVec_0_0\t";cout<<meshVec_0_0;
@@ -485,7 +522,7 @@ void drawScene() {
 			cout<<"\tmeshVec_1_0\t";cout<<meshVec_1_0;
 			cout<<"\tmeshVec_1_1\t";cout<<meshVec_1_1;*/
 
-			/*if (x > 0 & x< sizeOfMesh*expandMeshSize*imageToScenePixelDiff) {
+			/*if (x > 0 & x< maxXSize*imageToScenePixelDiff) {
 			//if (x > 0) {
 				//vAT3.ULVerInst[2] = grayLevel*depthScalingFactor+initalZ;
 				vAT3.ULVerInst[2] = priorAdjustedGrayLevel+initalZ;
@@ -512,7 +549,7 @@ void drawScene() {
 			//vAT3.BRVerInst[2] (int)image.at<Vec3b>(y,x).val[0]
 
 			/*cout << "iteration y:";cout<<y;cout<<" x:";cout<<x;cout<<"\n";
-			cout<<"sizeOfMesh/2: ";cout<<sizeOfMesh/2;cout<<"\ty>=sizeOfMesh/2:\t";cout<<(y>=sizeOfMesh/2);cout<<"\t(x>=sizeOfMesh/2):\t";cout<<(x>=sizeOfMesh/2);cout<<"\n";
+			cout<<"maxYSize/2: ";cout<<maxYSize/2;cout<<"\ty>=maxYSize/2:\t";cout<<(y>=maxYSize/2);cout<<"\t(x>=maxYSize/2):\t";cout<<(x>=maxYSize/2);cout<<"\n";
 			cout<<"UL_X:   ";cout << vAT3.ULVerInst[0];cout<<"\n";
 			cout<<"UL_Y:   ";cout << vAT3.ULVerInst[1];cout<<"\n";
 			cout<<"UL_Z:   ";cout << vAT3.ULVerInst[2];cout<<"\n";
@@ -549,9 +586,9 @@ void drawScene() {
 
 //Called every 25 milliseconds
 void update(int value) {
-	_angle += 0.2f;//0.1f;//0.330f;
-	if (_angle > -40) {
-		_angle -= 10;
+	_angle += 1.0f;//0.1f;//0.330f;
+	if (_angle > 50) {
+		_angle -= 100;
 	}
 	glutPostRedisplay();
 	glutTimerFunc(25, update, 0);
