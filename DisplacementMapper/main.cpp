67d2c7c6f3007af8c296fad6157450024e2b5d4d
Nate Sutton <natemsutton@gmail.com>
2015-07-16 21:49:23 -0400

    texture colors now loading correctly

--- DisplacementMapper/main.cpp
+++ DisplacementMapper/main.cpp
@@ -18,6 +18,7 @@
  */
 /* File for "Textures" lesson of the OpenGL tutorial on
  * www.videotutorialsrock.com
+ * load texture code from http://www.opengl-tutorial.org/beginners-tutorials/tutorial-5-a-textured-cube/#Compressed_Textures
  */
 
 #include <opencv2/core/core.hpp>
@@ -56,6 +57,10 @@ void handleKeypress(unsigned char key, int x, int y) {
 		exit(0);
 	}
 }
+
+#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
+#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
+#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
 
 char texGroup[200][150];
 String textureFile1 = texGroup[0];
@@ -161,7 +166,7 @@ bool is_nan( const T &value )
 }
 
 //Makes the image into a texture, and returns the id of the texture
-/*GLuint loadTexture(Image* image) {
+GLuint loadTexture(Image* image) {
 	//dispMapImage = imread(imageName6, CV_LOAD_IMAGE_COLOR);
 
 	GLuint textureId;
@@ -180,16 +185,189 @@ bool is_nan( const T &value )
 	return textureId; //Returns the id of the texture
 }
 
-GLuint _textureId; //The id of the texture*/
+GLuint _textureId; //The id of the texture
 
 
 
 //texture[200] texGroup2;
 //Image* texture[200];
 
+GLuint loadDDS(const char * imagepath){
+
+	unsigned char header[124];
+
+	FILE *fp;
+
+	/* try to open the file */
+	fp = fopen(imagepath, "rb");
+	if (fp == NULL){
+		printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar();
+		return 0;
+	}
+
+	/* verify the type of file */
+	char filecode[4];
+	fread(filecode, 1, 4, fp);
+	if (strncmp(filecode, "DDS ", 4) != 0) {
+		fclose(fp);
+		return 0;
+	}
+
+	/* get the surface desc */
+	fread(&header, 124, 1, fp);
+
+	unsigned int height      = *(unsigned int*)&(header[8 ]);
+	unsigned int width	     = *(unsigned int*)&(header[12]);
+	unsigned int linearSize	 = *(unsigned int*)&(header[16]);
+	unsigned int mipMapCount = *(unsigned int*)&(header[24]);
+	unsigned int fourCC      = *(unsigned int*)&(header[80]);
+
+
+	unsigned char * buffer;
+	unsigned int bufsize;
+	/* how big is it going to be including all mipmaps? */
+	bufsize = mipMapCount > 1 ? linearSize * 2 : linearSize;
+	buffer = (unsigned char*)malloc(bufsize * sizeof(unsigned char));
+	fread(buffer, 1, bufsize, fp);
+	/* close the file pointer */
+	fclose(fp);
+
+	unsigned int components  = (fourCC == FOURCC_DXT1) ? 3 : 4;
+	unsigned int format;
+	switch(fourCC)
+	{
+	case FOURCC_DXT1:
+		format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+		break;
+	case FOURCC_DXT3:
+		format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+		break;
+	case FOURCC_DXT5:
+		format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+		break;
+	default:
+		free(buffer);
+		return 0;
+	}
+
+	// Create one OpenGL texture
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+
+	unsigned int blockSize = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16;
+	unsigned int offset = 0;
+
+	/* load the mipmaps */
+	for (unsigned int level = 0; level < mipMapCount && (width || height); ++level)
+	{
+		unsigned int size = ((width+3)/4)*((height+3)/4)*blockSize;
+		glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height,
+			0, size, buffer + offset);
+
+		offset += size;
+		width  /= 2;
+		height /= 2;
+
+		// Deal with Non-Power-Of-Two textures. This code is not included in the webpage to reduce clutter.
+		if(width < 1) width = 1;
+		if(height < 1) height = 1;
+
+	}
+
+	free(buffer);
+
+	return textureID;
+
+
+}
+
+GLuint loadBMP_custom(const char * imagepath){
+
+	printf("Reading image %s\n", imagepath);
+
+	// Data read from the header of the BMP file
+	unsigned char header[54];
+	unsigned int dataPos;
+	unsigned int imageSize;
+	unsigned int width, height;
+	// Actual RGB data
+	unsigned char * data;
+
+	// Open the file
+	FILE * file = fopen(imagepath,"rb");
+	if (!file)							    {printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar(); return 0;}
+
+	// Read the header, i.e. the 54 first bytes
+
+	// If less than 54 bytes are read, problem
+	if ( fread(header, 1, 54, file)!=54 ){
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	// A BMP files always begins with "BM"
+	if ( header[0]!='B' || header[1]!='M' ){
+		printf("Not a correct BMP file\n");
+		return 0;
+	}
+	// Make sure this is a 24bpp file
+	if ( *(int*)&(header[0x1E])!=0  )         {printf("Not a correct BMP file\n");    return 0;}
+	if ( *(int*)&(header[0x1C])!=24 )         {printf("Not a correct BMP file\n");    return 0;}
+
+	// Read the information about the image
+	dataPos    = *(int*)&(header[0x0A]);
+	imageSize  = *(int*)&(header[0x22]);
+	width      = *(int*)&(header[0x12]);
+	height     = *(int*)&(header[0x16]);
+
+	// Some BMP files are misformatted, guess missing information
+	if (imageSize==0)    imageSize=width*height*3; // 3 : one byte for each Red, Green and Blue component
+	if (dataPos==0)      dataPos=54; // The BMP header is done that way
+
+	// Create a buffer
+	data = new unsigned char [imageSize];
+
+	// Read the actual data from the file into the buffer
+	fread(data,1,imageSize,file);
+
+	// Everything is in memory now, the file wan be closed
+	fclose (file);
+
+	// Create one OpenGL texture
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	// Give the image to OpenGL
+	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+	// Poor filtering, or ...
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	// ... nice trilinear filtering.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, data );
+	//glGenerateMipmap(GL_TEXTURE_2D);
+
+	// OpenGL has now copied the data. Free our own version
+	delete [] data;
+
+	// Return the ID of the texture we just created
+	return textureID;
+}
+
 GLuint LoadTexture2( const char * filename, int width, int height )
 {
-	GLuint texture;
+	/*GLuint texture;
 	unsigned char * data;
 	FILE * file;
 
@@ -198,7 +376,25 @@ GLuint LoadTexture2( const char * filename, int width, int height )
 	if ( file == NULL ) return 0;
 	data = (unsigned char *)malloc( width * height * 3 );
 	fread( data, width * height * 3, 1, file );
-	fclose( file );
+	fclose( file );*/
+
+    GLuint texture;
+    unsigned char *data;
+    FILE *file;
+
+    // open texture data
+    file = fopen(filename, "rb");
+    if (file == NULL) return 0;
+
+    // allocate buffer
+    data = (unsigned char*) malloc(width * height * 4);
+
+    // read texture data
+    fread(data, width * height * 4, 1, file);
+    fclose(file);
+
+    // allocate a texture name
+    glGenTextures(1, &texture);
 
 	glGenTextures( 1, &texture ); //generate the texture with the loaded data
 	glBindTexture( GL_TEXTURE_2D, texture ); //bind the textureto it’s array
@@ -225,16 +421,16 @@ GLuint LoadTexture2( const char * filename, int width, int height )
 	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
 
 	//Generate the texture with mipmaps
-	gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data );
+	gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, data );
 	free( data ); //free the texture
 	return texture; //return whether it was successfull
 }
 
 void initRendering() {
 	//changed
-	/*glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);*/
+	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
 	glShadeModel(GL_SMOOTH); //Enable smooth shading
@@ -243,6 +439,11 @@ void initRendering() {
 
 	//Image* image = loadBMP(dispMapGroup[(int)timeInMs]);
 	//delete image;
+	//Image* image = loadBMP("idptest.bmp");
+	//Image* image = loadBMP(texGroup[(int)texFileCounter]);
+	Image* image = loadBMP("vtr.bmp");
+	_textureId = loadTexture(image);
+	delete image;
 }
 
 
@@ -700,28 +901,43 @@ void drawScene() {
 
 	GLfloat directedLight[] = {200.7f, 200.7f, 200.7f, 1.0f};
 	GLfloat directedLightPos[] = {-210.0f, 215.0f, 220.0f, 0.0f};
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, directedLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, directedLightPos);
 	//GLfloat ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};
 	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
 
-	/*GLfloat lightColor[] = {0.7f, 0.7f, 0.7f, 1.0f};
-	GLfloat lightPos[] = {-2 * BOX_SIZE, BOX_SIZE, 4 * BOX_SIZE, 1.0f};
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);*/
-	//glLightfv(GL_LIGHT0, GL_DIFFUSE, directedLight);
-	//glLightfv(GL_LIGHT0, GL_POSITION, directedLightPos);
+	//GLfloat lightColor[] = {0.7f, 0.7f, 0.7f, 1.0f};
+	//GLfloat lightPos[] = {-2 * BOX_SIZE, BOX_SIZE, 4 * BOX_SIZE, 1.0f};
+	//glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+	//glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
 	glRotatef(-_angle, rotationX, rotationY, rotationZ);
 
 	glEnable(GL_TEXTURE_2D);
+	//glBindTexture(GL_TEXTURE_2D, _textureId)
+	/*Image* image = loadBMP("idptest.bmp");
+	_textureId = loadTexture(image);
+	delete image;
+	glBindTexture(GL_TEXTURE_2D, "vtr.bmp");*/
 	//numberOfTexs
 	//glBindTexture( GL_TEXTURE_2D, LoadTexture2(texGroup[(int)timeInMs], 512, 512) );
-	glBindTexture( GL_TEXTURE_2D, LoadTexture2(texGroup[(int)texFileCounter], 512, 512) );
+	//glBindTexture( GL_TEXTURE_2D, LoadTexture2(texGroup[(int)texFileCounter], 512, 512) );
+	//glBindTexture( GL_TEXTURE_2D, LoadTexture2("vtr.bmp", 512, 512) );
+	//glBindTexture( GL_TEXTURE_2D, LoadTexture2("MyRawImage.raw", 512, 512) );
+	//glBindTexture( GL_TEXTURE_2D, loadTexture(texGroup[(int)texFileCounter], 512, 512) );
+
+	//GLuint Texture = loadDDS(texGroup[(int)texFileCounter]);
+	//GLuint Texture = loadDDS("newVtr.dds");
+	//GLuint Texture = loadBMP_custom("vtr.bmp");
+	//glBindTexture(GL_TEXTURE_2D, Texture);
+	//GLuint NormalTexture = loadBMP_custom("vtr.bmp");
+	glBindTexture(GL_TEXTURE_2D, loadBMP_custom(texGroup[(int)texFileCounter]));
 
 	//Bottom
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	//glColor3f(1.0f, 1.0f, 1.0f);
-	glColor3f(1.0f, 0.45, 0.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	//glColor3f(1.0f, 0.45, 0.0f);
 	glBegin(GL_QUADS);
 
 	glNormal3f(1.0f, 1.0f, 1.0f);
