f02d52d25347400be584646d82d791275b44ea6e
Nate Sutton <natemsutton@gmail.com>
2015-08-04 23:24:44 -0400

    Removed loadTexture method no longer used

--- DisplacementMapper/main.cpp
+++ DisplacementMapper/main.cpp
@@ -146,28 +146,6 @@ bool is_nan( const T &value )
 	return value != value;
 }
 
-//Makes the image into a texture, and returns the id of the texture
-GLuint loadTexture(Image* image) {
-	//dispMapImage = imread(imageName6, CV_LOAD_IMAGE_COLOR);
-
-	GLuint textureId;
-	glGenTextures(1, &textureId); //Make room for our texture
-	glBindTexture(GL_TEXTURE_2D, textureId); //Tell OpenGL which texture to edit
-	//Map the image to the texture
-	glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
-			0,                            //0 for now
-			GL_RGB,                       //Format OpenGL uses for image
-			image->width, image->height,  //Width and height
-			0,                            //The border of the image
-			GL_RGB, //GL_RGB, because pixels are stored in RGB format
-			GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
-			//as unsigned numbers
-			image->pixels);               //The actual pixel data
-	return textureId; //Returns the id of the texture
-}
-
-GLuint _textureId; //The id of the texture
-
 //texture[200] texGroup2;
 Image* texture[200];
 
